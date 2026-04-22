e9dcd2bd749fd9a40e6650b3c03bd981684d3276
Ignacio Murube <IgnacioMurubeCrego@users.noreply.github.com>
2026-04-22 22:47:45 +0200

    back - chore: init Spring Boot project

---
+++ backend/src/main/java/com/cloudrower/api/CloudrowerApiApplication.java
@@ -0,0 +1,11 @@
+package com.cloudrower.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudrowerApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CloudrowerApiApplication.class, args);
+	}
+}
