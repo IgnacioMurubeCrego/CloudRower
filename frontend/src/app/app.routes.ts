a390748237f0b2368ad804303b115dbe0e5e8f06
Ignacio Murube <IgnacioMurubeCrego@users.noreply.github.com>
2026-04-22 23:25:50 +0200

    front - feat: configure lazy loading routes in app.routes.ts

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,26 @@
+// src/app/app.routes.ts
+import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth-guard';
+
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./features/auth/auth.routes').then(m => m.AUTH_ROUTES)
+  },
+  {
+    path: 'dashboard',
+    canActivate: [authGuard],
+    loadChildren: () =>
+      import('./features/dashboard/dashboard.routes').then(m => m.DASHBOARD_ROUTES)
+  },
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'auth/login'
+  }
+];
