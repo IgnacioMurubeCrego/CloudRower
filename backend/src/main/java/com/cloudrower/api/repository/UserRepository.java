b846debdb4fd403ade430187e2f9dff851c03eb7
Ignacio Murube <IgnacioMurubeCrego@users.noreply.github.com>
2026-04-22 23:00:39 +0200

    back - feat: add UserRepository with findByEmail search method

---
+++ backend/src/main/java/com/cloudrower/api/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.cloudrower.api.repository;
+
+import com.cloudrower.api.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, String> {
+
+    Optional<UserEntity> findByEmail(String email);
+}
