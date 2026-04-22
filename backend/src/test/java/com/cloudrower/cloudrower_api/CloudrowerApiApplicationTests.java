15e8acfac57254463fc7e704649873cd257d6363
Ignacio Murube <IgnacioMurubeCrego@users.noreply.github.com>
2026-04-22 23:53:09 +0200

    chore: remove Angular root artifacts after reorganization to frontend/

---
+++ backend/src/test/java/com/cloudrower/cloudrower_api/CloudrowerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudrower.cloudrower_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudrowerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
